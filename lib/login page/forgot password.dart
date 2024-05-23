c996af97323de61d85a64f3c0affa4faef467b31
takimoru <nicolastz127@gmail.com>
2024-05-23 10:11:36 +0800

    Page Forgot password kelar

--- lib/login page/forgot password.dart
+++ lib/login page/forgot password.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:revuemurale/homepage/homepage.dart';
+import 'package:revuemurale/login page/login page.dart';
 
 class ForgotPassword extends StatefulWidget {
+  const ForgotPassword({super.key});
+
   @override
   _ForgotPasswordState createState() => _ForgotPasswordState();
 }
@@ -9,22 +11,16 @@ class ForgotPassword extends StatefulWidget {
 class _ForgotPasswordState extends State<ForgotPassword> {
   final _formKey = GlobalKey<FormState>();
   final TextEditingController _usernameController = TextEditingController();
-  final TextEditingController _passwordController = TextEditingController();
 
   @override
   void dispose() {
     _usernameController.dispose();
-    _passwordController.dispose();
     super.dispose();
   }
 
-  void _login() {
+  void _send() {
     if (_formKey.currentState!.validate()) {
       // Perform login logic and navigate to HomePage
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => HomePage()),
-      );
     }
   }
 
@@ -32,7 +28,7 @@ class _ForgotPasswordState extends State<ForgotPassword> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Column(
+        title: const Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text('Welcome Back Students'),
@@ -47,18 +43,32 @@ class _ForgotPasswordState extends State<ForgotPassword> {
           ],
         ),
       ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(top  : 50.0),
-            child: Image.asset(
-              'assets/images/orang.jpg', // Replace with the path to your image
-              height:150.0, // Set the desired height for the image
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 10.0),
+              child: Column(
+                children: [
+                  Image.asset(
+                    'assets/images/orang2.jpg', // Replace with the path to your image
+                    height: 200.0, // Set the desired height for the image
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Text(
+                      'Please enter your email address to receive a link to create a new password via email.',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 16.0,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-          Expanded(
-            child: Padding(
-              padding: EdgeInsets.all(16.0),
+            Padding(
+              padding: const EdgeInsets.all(15.0),
               child: Form(
                 key: _formKey,
                 child: Column(
@@ -66,73 +76,58 @@ class _ForgotPasswordState extends State<ForgotPassword> {
                   children: <Widget>[
                     TextFormField(
                       controller: _usernameController,
-                      decoration: InputDecoration(
-                        labelText: 'Username',
+                      decoration: const InputDecoration(
+                        labelText: 'Email',
                         border: OutlineInputBorder(),
                       ),
-                      keyboardType: TextInputType.phone,
+                      keyboardType: TextInputType.emailAddress,
                       validator: (value) {
                         if (value == null || value.isEmpty) {
-                          return 'Mohon mengisi No.Handphone';
+                          return 'Mohon mengisi Email';
                         }
-                        if (int.tryParse(value) == null) {
-                          return 'Mohon mengisi No.Handphone yang valid';
+                        if (!RegExp(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$').hasMatch(value)) {
+                          return 'Mohon mengisi dengan Email kampus';
                         }
                         return null;
                       },
                     ),
-                    SizedBox(height: 10.0),
-                    TextFormField(
-                      controller: _passwordController,
-                      decoration: InputDecoration(
-                        labelText: 'Password',
-                        border: OutlineInputBorder(),
-                      ),
-                      obscureText: true,
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Mohon memasukkan password yang sesuai';
-                        }
-                        return null;
-                      },
-                    ),
-                    SizedBox(height: 30.0),
+                    const SizedBox(height: 30.0),
                     ElevatedButton(
-                      onPressed: _login,
+                      onPressed: _send,
                       style: ElevatedButton.styleFrom(
                         backgroundColor: Colors.blue, // Set the background color to blue
                       ),
-                      child: Text('Send', style: TextStyle(color: Colors.white)),
+                      child: const Text('Send', style: TextStyle(color: Colors.white)),
                     ),
-                    SizedBox(height: 10.0), // Add some spacing between the button and the redirect text
+                    const SizedBox(height: 16.0), // Add some spacing between the button and the redirect text
                     GestureDetector(
                       onTap: () {
-                        // Navigate to the signup or forgot password page
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => LoginPage()),
+                        ); // Navigate to the login page
                       },
-                      child: Text(
-                        'Forget Password',
-                        style: TextStyle(
-                          color: Colors.blue,
-                          decoration: TextDecoration.underline,
-                        ),
+                      child: const Text(
+                        'Back to Login',
+                        style: TextStyle(color: Colors.blue, decoration: TextDecoration.underline),
                       ),
                     ),
                   ],
                 ),
               ),
             ),
-          ),
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: Padding(
-              padding: const EdgeInsets.only(bottom: 16.0),
-              child: Image.asset(
-                'assets/images/Mulia_logo.png', // Replace with the path to your image
-                height: 80.0, // Set the desired height for the image
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Padding(
+                padding: EdgeInsets.only(bottom: 16.0),
+                child: Image.asset(
+                  'assets/images/Mulia_logo.png', // Replace with the path to your image
+                  height: 80.0, // Set the desired height for the image
+                ),
               ),
             ),
-          )
-        ],
+          ],
+        ),
       ),
     );
   }
